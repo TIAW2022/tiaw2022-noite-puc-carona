bd7734c8eb11289f897aa1624f14f54e43589250
pablo-lorran <116111536+pablo-lorran@users.noreply.github.com>
2022-11-18 18:55:20 -0300

    Update app.js

--- codigo/app.js
+++ codigo/app.js
@@ -27,6 +27,8 @@ function incluirContato (){
     let strSenha = document.getElementById ('password').value;
     let strEmail = document.getElementById('email').value;
     let strNascimento = document.getElementById('datanascimento').value;
+    let strGenero = document.querySelector('input[name="gender"]:checked').value;
+
     
 
         
@@ -34,6 +36,7 @@ function incluirContato (){
     
     let novoContato = {
         nome: strNome,
+        genero: strGenero,
         email: strEmail,
         nascimento: strNascimento,
         senha: strSenha
@@ -51,78 +54,3 @@ function incluirContato (){
 // Configura os botões
 
 document.getElementById ('btn_submit').addEventListener ('click', incluirContato);
-/*function leDados(){
-    let strDados = localStorage.getItem('db');
-    let objDados = {};
-
-    if(strDados){
-        objDados = JSON.parse (strDados);
-    }
-    else{
-        '[]';
-    }
-
-    return objDados;
-}
-
-function salvaDados(dados){
-
-    localStorage.setItem('db', JSON.stringify(dados));
-
-}
-
-function incluirDados(){
-    let objDados = leDados();
-
-    let strNome = document.getElementById('completename').value;
-    let strNascimento = document.getElementById('compldatanascimentoetename').value;
-    let strEmail = document.getElementById('email').value;
-    let strCpf = document.getElementById('cpf').value;
-    let strCelular = document.getElementById('celular').value;
-    let strNumeroPessoa = document.getElementById('numeropessoa').value;
-    let strCnh = document.getElementById('cnh').value;
-    let strPlaca = document.getElementById('placa').value;
-    let strSenha = document.getElementById('password').value;
-
-    let novoUsuario = {
-        usertype: strUsertype,
-        nome: strNome,
-        nascimento: strNascimento,
-        email: strEmail,
-        cpf: strCpf,
-        celular: strCelular,
-        numeroPessoa: strNumeroPessoa,
-        cnh: strCnh,
-        placa: strPlaca,
-        senha: strSenha
-        
-    };
-
-    objDados.usuarios.push (novoUsuario);
-    salvaDados (objDados);
-
-
-}
-
-document.getElementById('btn_submit').addEventListener('click', incluirDados );*/
-
-
-/*function cadastrar(){
-    let objDados = JSON.parse(localStorage.getItem('objDados') || '[]');
-    objDados.push(
-        {
-        usertype: usertype.value,
-        nome: completename.value,
-        nascimento: datanascimento.value,
-        email: email.value,
-        cpf: cpf.value,
-        celular: celular.value,
-        numeroPessoa: numeropessoa.value,
-        cnh: cnh.value,
-        placa: placa.value,
-        senha: senha.value
-        }
-    );
-
-    localStorage.setItem('objDados', JSON.stringify(objDados));
-}*/
