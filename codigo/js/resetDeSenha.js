b253264277dd8151711266184bd50908a5c52ee0
MCarolineAlves <116688976+MCarolineAlves@users.noreply.github.com>
2022-12-18 21:20:32 -0300

    Update resetDeSenha.js

--- codigo/js/resetDeSenha.js
+++ codigo/js/resetDeSenha.js
@@ -2,7 +2,7 @@ function ola() {
   alert("Redefinição solicitada com sucesso!");
 }
 
-document.querySelector(".click").addEventListener("click", (event) => { //escuta o envio e armazena o valor
+document.querySelector(".click").addEventListener("click", (event) => { //recebe o envio e armazena o valor
   event.preventDefault();
 
 })
